22a32a532b86ba80e8c5bb67817817d08c3baa30
Darya Vaitsiakhouskaya <darya.vaitsiakhouskaya@mail.ru>
2023-03-15 19:28:26 +0300

    ДЗ 4 к семинару по С#

---
+++ HW4/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.Write("Введите число 1: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число 2: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = number2;
+// {
+// for (int i=1; i<=number2; i++);
+// }
+
+// Console.WriteLine($"{Math.Pow(number,result)}");
+
+// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Counting (int number)
+// {
+// int sum= 0;
+// while(number>0)
+//     {
+//         sum+=number%10;
+//         number=number/10;
+//     }
+//     return sum;
+// }
+
+// int result = Counting(number);
+// System.Console.WriteLine($"Сумма чисел в числе{number} = {result}");
+
+// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[ ]arr = {1, 2, 5, 7, 19};
+// void PrintArray(int[]array)
+// {
+//     int count=array.Length;
+//     for (int i=0; i<count; i++)
+//     {
+//         Console.Write($"{array[i]}"," ");
+//     }
+// }
+// PrintArray(arr);
